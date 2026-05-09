4a278f54ca607b8b65862e805203d44276030e64
Archie Calvert <archie.calvert@outlook.com>
2026-05-09 18:13:24 +0100

    done 1.2

---
+++ baselinecode/plots/1.2.py
@@ -0,0 +1,59 @@
+import subprocess
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.ticker as ticker
+
+# ../matrix_multiply.out <L> <M> <N> <SEED>
+PROGRAM_RUN = "../matrix_multiply.out {} {} {} {}"
+SEED = 1234
+MIN_DIM = 64
+MAX_DIM = 1024
+STEP = 16
+REPEAT_COUNT = 5
+
+def run_test(L: int, M: int, N: int) -> list[float]:
+    """
+    Runs the experiment for the given matrix inputs.
+
+    Returns:
+        list[float]: the resulting data of [L, M, N, TIME]
+    """
+    # run program and get output
+    std_args = subprocess.run(PROGRAM_RUN.format(max(L, 1), max(M, 1), max(N, 1), SEED), shell=True, capture_output=True, text=True)
+    raw_result = std_args.stdout
+
+    # break each statistic into array
+    stats_strings = [x.strip() for x in raw_result.split(",")]
+
+    # extract the final section of the stat, being the actual measured value
+    stats_vars = [float(x.split(" ")[-1]) for x in stats_strings]
+    return stats_vars
+
+
+if __name__ == "__main__":
+    data = []
+    for i in range(MIN_DIM, MAX_DIM + 1, STEP):
+        print(f"Running Experiment on M={i}")
+        time_total = 0.0
+        L = M = N = 0
+        for j in range(REPEAT_COUNT):
+            res = run_test(i, i, i)
+            L, M, N, time = int(res[0]), int(res[1]), int(res[2]), res[3]
+            time_total += time
+        time = time_total / float(REPEAT_COUNT)
+
+        flops = float(L * N * (2 * M - 1)) / time if time != 0 else 0
+        data.append([flops, L])
+
+    df = pd.DataFrame(data, columns=["FLOPS", "Dimension"])
+    x = df["Dimension"]
+    y = df["FLOPS"]
+    
+    figure, axis = plt.subplots()
+    axis.set_xlabel("Matrix Dimension N")
+    axis.set_ylabel("FLOPS")
+    axis.grid(True, alpha=0.6)
+    axis.plot(x, y, marker='o', linewidth=2, markersize=5)
+    plt.savefig(fname = "graphs/1.2.png", dpi=200)
+
