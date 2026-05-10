ec5701f51ee70c10e0b940ce8d8eec0bd121d12b
Archie Calvert <archie.calvert@outlook.com>
2026-05-10 11:42:24 +0100

    fixed 1.2

--- baselinecode/plots/1.2.py
+++ baselinecode/plots/1.2.py
@@ -7,9 +7,10 @@ import os
 from pathlib import Path
 
 DIR = Path(__file__).stem
+MODE = 1
 
-# ../matrix_multiply.out <L> <M> <N> <SEED>
-PROGRAM_RUN = "../matrix_multiply.out {} {} {} {} {}"
+# ../matrix_multiply.out <L> <M> <N> <SEED> <MODE> <ADDITIONAL ARGS>
+PROGRAM_RUN = "../matrix_multiply.out {} {} {} {} {} {}"
 SEED = 1234
 MIN_DIM = 64
 MAX_DIM = 1024
@@ -24,7 +25,7 @@ def run_test(L: int, M: int, N: int) -> list[float]:
         list[float]: the resulting data of [L, M, N, TIME]
     """
     # run program and get output
-    std_args = subprocess.run(PROGRAM_RUN.format(max(L, 1), max(M, 1), max(N, 1), SEED, 1), shell=True, capture_output=True, text=True)
+    std_args = subprocess.run(PROGRAM_RUN.format(max(L, 1), max(M, 1), max(N, 1), SEED, MODE, 1, 1), shell=True, capture_output=True, text=True)
     raw_result = std_args.stdout
 
     # break each statistic into array
